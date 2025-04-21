e233b1fc0edfcfbe1b0314be823eff87a0173720
Robin Kåveland <robin@kaveland.no>
2025-04-21 12:08:37 +0200

    Work on map tooltips

--- kollektivkart/mapview.py
+++ kollektivkart/mapview.py
@@ -5,17 +5,51 @@ from flask import g
 
 from . import queries
 
+def hovertooltip() -> (list[pd.Series], str):
+    hover_data = [
+        "name",
+        "month",
+        "hour",
+        "air_distance_km",
+        "rush_intensity",
+        "hourly_duration",
+        "monthly_duration",
+        "hourly_delay",
+        "monthly_delay",
+        "hourly_deviation",
+        "monthly_deviation",
+        "hourly_count",
+        "monthly_count"
+    ]
+    by_ix = {
+        col: i for i, col in enumerate(hover_data)
+    }
+    def col(c): return f"%{{customdata[{by_ix[c]}]}}"
+
+    tooltip = f"""
+{col("name")} distance {col("air_distance_km")}km<br><br>
+Data from {col("hour")}:00 to {col("hour")}:59 for {col("month")}<br><br>
+{col("monthly_count")} vehicles recorded for this month and {col("hourly_count")} in this hour.<br><br>
+Typically {col("monthly_duration")}s travel time, {col("hourly_duration")}s in this hour for rush intensity of {col("rush_intensity")}<br><br>
+Typical monthly delay is {col("monthly_delay")}s, {col("hourly_delay")}s for this hour<br><br>
+Typical monthly deviation is {col("monthly_deviation")}s, {col("hourly_deviation")}s for this hour<br><br>
+"""
+
+    return hover_data, tooltip
+
+
 
 def draw_map(
     df: pd.DataFrame, center: dict[str, float], zoom: int, range_color_scale_stop=2.5
 ):
-    return px.scatter_map(
+    hover_data, tooltip = hovertooltip()
+    fig = px.scatter_map(
         df,
         lat="lat",
         lon="lon",
         size="hourly_count",
         hover_name="name",
-        hover_data=["monthly_count", "hourly_duration", "monthly_duration"],
+        hover_data=hover_data,
         center=center,
         zoom=zoom,
         height=800,
@@ -23,6 +57,8 @@ def draw_map(
         color_continuous_scale="Cividis_r",
         range_color=[1.0, range_color_scale_stop],
     )
+    fig.update_traces(hovertemplate=tooltip)
+    return fig
 
 
 def or_empty(dict_or_none: None | dict[str, object]) -> dict[str, object]:
