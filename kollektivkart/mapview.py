967ab5bbb1358965ff974b3edb10f2834dda1878
Karl Yngve Lervåg <karl.yngve+git@gmail.com>
2025-05-23 21:49:48 +0200

    Update minor things

--- kollektivkart/mapview.py
+++ kollektivkart/mapview.py
@@ -6,7 +6,7 @@ from flask import g
 from . import queries
 
 
-def hovertooltip(hour: int) -> (list[str], str):
+def hovertooltip(hour: int) -> tuple[list[str], str]:
     hover_data = [
         "name",
         "air_distance_meters",
